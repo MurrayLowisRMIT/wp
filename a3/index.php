577c0580030d44ce9fd2055c64e651c7ceb0d162
Murray <s3862651@student.rmit.edu.au>
2021-03-01 12:52:00 +1100

    Improved navModule and $_get functions, added ID element to article arrays

--- a3/index.php
+++ a3/index.php
@@ -2,56 +2,9 @@
     session_start();
     require_once('tools.php');
     topModule();
+    navModule();
 ?>
 
-<article class="foreword">
-    <div class="articleHeader">
-        <h2 class="basic">Foreword</h2>
-    </div>
-    <p>Hello and welcome,
-    </p><p>This year is the centenary of the birth of the ANZAC legend. As such, many people, particularly young people, are looking for ways to connect with people of that period, in particular those who created the ANZAC legend.
-    </p><p>This site presents the letters of Douglas Raymond Baker, who came from Gympie, Queensland, Australia. He enlisted in August 1914 and during the years that followed, he wrote letters and post cards to his family at home. In them, he describes much of the goings-on of the life of an ANZAC, his feelings and opinions, and experiences while visiting his relatives in England during leave.
-    </p><p>They start from the beginning of basic training in Brisbane in August 1914 and end in May 1918.
-    </p><p>They are offered here so that others may get an understanding of life as an ANZAC and get a sense of what life on the battlefield was like.
-    </p><p>These are copies of letters written by my father Douglas Raymond Baker during the First World War (1914-1918) to his family in Gympie. I have three thick exercise books in which the letters were copied in handwriting by my Aunt Alice, his sister. I understand that this was done so his letters could be sent on to other members of the family. I don’t know if the originals are still in existence, probably not.
-    </p><p>In the early letters Alice has not included the name of the person they were written to but later she writes at the beginning, “Letter to Dot”, “Letter to Mother”, etc. and later still copies the original, “Dear Al”, “Dear Mag”, etc. Likewise the endings are often not laid out fully as we would do but Al may have done this or Dad may have been saving space on the page.
-    </p><p>Some letters are slightly out of date order in the books but as most of these are around the time his father died I have put them in the correct order as this is crucial to understanding the sense of the contents.  A lot will be missing – many, many ships were sunk but thanks to Aunt Alice we have these copies of the ones that did get through.
-    </p><p>Where Dad used brackets in a letter I have used [ ] style and where I have made any comment or explanation I have used ( ) and Italics.
-    </p><p>Where he uses the word “gay” it is used in the true sense, bright, happy, carefree, etc. This is the original meaning of the word before it became associated with the homosexual community.
-    </p><p>The amounts of money are, of course, in Pounds, shillings and pence but as these are out of date and we have no pound sign in the computer I have written them in, sometimes showing the decimal equivalents. One Pound equalled $2, one shilling equalled 10 cents and one penny equalled a little under one cent. After a while I stopped putting the equivalents in as they had no significance unless you knew the relative cost of things then and now. A shilling – equivalent to ten cents might actually buy ten or twenty dollars worth of goods now.
-    </p>
-</article>
+<?= articleBuilder(); ?>
 
-<?= articleBuilder(
-    $lettersArray[$currentArticle][DateStart],
-    $lettersArray[$currentArticle][DateEnd],
-    $lettersArray[$currentArticle][Type],
-    $lettersArray[$currentArticle][Town],
-    $lettersArray[$currentArticle][Country],
-    $lettersArray[$currentArticle][Transport],
-    $lettersArray[$currentArticle][Battle],
-    $lettersArray[$currentArticle][Content]
-); ?>
-
-
-<article id="form">
-    <div class="articleHeader">
-        <h2 class="basic">Contact Me</h2>
-    </div>
-    <form action="https://titan.csit.rmit.edu.au/~e54061/wp/testcontact.php" method="post">
-        <label for="name">Name</label>
-        <input type="text" id="name" name="name" placeholder="Name">
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email" placeholder="Email">
-        <label for="mobile">Mobile</label>
-        <input type="subject" id="mobile" name="mobile" placeholder="Mobile">
-        <label for="subject">Subject</label>
-        <input type="message" id="subject" name="subject" placeholder="Subject">
-        <label class="messageField" for="message">Message</label>
-        <textarea class="messageField basic" id="message" name="message" cols='80' rows='9'></textarea>
-        <input class="checkBox" type="checkbox" id="remember-me" name="remember-me">
-        <label class="checkBoxLabel" for="remember-me">Remember me</label>
-        <input type="submit" name="send" value="Submit">
-    </form>
-</article>
 <?= endModule(); ?>
--- a3/tools.php
+++ a3/tools.php
@@ -22,15 +22,14 @@ function topModule() {
             https://ranzcrarchivesblog.wordpress.com/2015/04/13/anzac-images-radiology-at-gallipoli/-->
             <img class="floatLeft headerimg" src='../../media/ANZAC Crest.png' alt='ANZAC crest'>
             <img class="floatRight headerimg" src='../../media/ANZAC Crest.png' alt='ANZAC crest'>
-            <H1>ANZAC Douglas Raymond Baker<br><small>Letters Home</small></H1>
+            <h1>ANZAC Douglas Raymond Baker<br><small>Letters Home</small></h1>
         </header>
 OUTPUT;
     echo $html;
-    navModule();
 }
 
 function endModule() {
-    global $_SERVER;
+    //global $_SERVER;//!!!
     
     echo "</main>
 
@@ -54,15 +53,15 @@ function endModule() {
     </html>";
 }
 
-$currentArticle = "foreword";
-$currentArticle = $_POST["articleID"];
-
 //change 'fopen()' to actual address when setup complete -> "/home/eh1/e54061/public_html/wp/letters-home.txt"!!!
 if (($lettersCSV = fopen("letters-home.txt", "r")) && flock($lettersCSV, LOCK_SH) !== false) {
     $headings = fgetcsv($lettersCSV, 0, "\t");
+    $articleID = 0;
     while(($line = fgetcsv($lettersCSV, 0, "\t")) !== false) {
         $lineAssociative = array_combine($headings, $line);
+        $lineAssociative["ArticleID"]=$articleID;
         $lettersArray[] = $lineAssociative;
+        $articleID += 1;
     }
     flock($lettersCSV, LOCK_UN);
     fclose($lettersCSV);}
@@ -80,25 +79,25 @@ function navModule() {
         sort($years);
         
         if(!empty($line[Battle])) {
-            $articleKey[] = [$year,$line[Battle]." - ".substr($line[DateStart], 5, 5)];
+            $articleKey[] = [$year, $line[Battle]." - ".substr($line[DateStart], 5, 5), $line[ArticleID]];
         } else {
-            $articleKey[] = [$year,$line[Type]." - ".substr($line[DateStart], 5, 5)];
-        }        
+            $articleKey[] = [$year, $line[Type]." - ".substr($line[DateStart], 5, 5), $line[ArticleID]];
+        }
     }
-
+    
     echo "<nav>
         <ul>
-            <form method=\"post\">
+            <form method=\"GET\">
                 <li><input type=\"submit\" name=\"articleID\" value=\"Foreword\"/></li>
             </form>";
             foreach($years as $value) {
                 echo "<li><button class=\"button collapsible\">".$value."</button>
                     <div class=\"collapsibleContent\">
-                        <form method=\"post\">
+                        <form method=\"GET\">
                             <ul>";
-                            foreach($articleKey as $line) {
-                                if ($line[0] === $value) {
-                                    echo "<li><input type=\"submit\" name=\"articleID\" value=\"".$line[1]."\"/></li>";
+                            foreach($articleKey as $row) {
+                                if ($row[0] === $value) {
+                                    echo "<li><input type=\"submit\" name=\"articleID\" value=\"".$row[1]."\"/></li>";
                                 }
                             }
                             echo "</ul>
@@ -106,33 +105,91 @@ function navModule() {
                     </div>
                 </li>";
             }
-            echo "</ul>
+            echo "<form method=\"GET\">
+                <li><input type=\"submit\" name=\"articleID\" value=\"Contact us\"/></li>
+            </form>
+            </ul>
             <img class=\"floatLeft\" src='../../media/Douglas Raymond Baker portrait.jpg' alt='Portait of Douglas Baker' id=\"portrait\">
         </nav>
 
         <main>";
 }
-echo $lettersArray[0][DateStart];
 
-/*function articleBuilder($currentArticle) {
-    $contentFormatted = str_replace("\n", "</p><p>", $lettersArray[$articleID][Content]);
-    $html = <<<"OUTPUT"
-                <article>
-                    <div class="articleHeader">
-                        <h2>$lettersArray[$articleID][Battle]</h2>
-                        <h3>$lettersArray[$articleID][Town], $lettersArray[$articleID][Country] - $lettersArray[$articleID][DateStart]</h3>
-                    </div>
-                    <div class="$lettersArray[$articleID][Type]">
-                        <div>$lettersArray[$articleID][Type] placeholder cover text</div>
-                        <div><p>$contentFormatted</p></div>
-                    </div>
-                </article>
+function articleBuilder() {
+    /*global $lettersArray;
+    //$lettersArray[textual key][data (0-8)]//!!!
+    //global $_GET;//!!!
+    global $articleKey;
+    //$articleKey[$year, Battle/Type - DateStart, key]//!!!
+    
+    foreach($lettersArray as $line) {
+        $year = substr($line[DateStart], 0, 4);
+        if(!in_array($year, $years)) {
+            $years[] = $year;
+        }
+    }*/
+
+    if ($_GET["articleID"] === "Foreword") {
+        $html = <<<"OUTPUT"
+        <article class="foreword">
+            <div class="articleHeader">
+                <h2 class="basic">Foreword</h2>
+            </div>
+            <p>Hello and welcome,
+            </p><p>This year is the centenary of the birth of the ANZAC legend. As such, many people, particularly young people, are looking for ways to connect with people of that period, in particular those who created the ANZAC legend.
+            </p><p>This site presents the letters of Douglas Raymond Baker, who came from Gympie, Queensland, Australia. He enlisted in August 1914 and during the years that followed, he wrote letters and post cards to his family at home. In them, he describes much of the goings-on of the life of an ANZAC, his feelings and opinions, and experiences while visiting his relatives in England during leave.
+            </p><p>They start from the beginning of basic training in Brisbane in August 1914 and end in May 1918.
+            </p><p>They are offered here so that others may get an understanding of life as an ANZAC and get a sense of what life on the battlefield was like.
+            </p><p>These are copies of letters written by my father Douglas Raymond Baker during the First World War (1914-1918) to his family in Gympie. I have three thick exercise books in which the letters were copied in handwriting by my Aunt Alice, his sister. I understand that this was done so his letters could be sent on to other members of the family. I don’t know if the originals are still in existence, probably not.
+            </p><p>In the early letters Alice has not included the name of the person they were written to but later she writes at the beginning, “Letter to Dot”, “Letter to Mother”, etc. and later still copies the original, “Dear Al”, “Dear Mag”, etc. Likewise the endings are often not laid out fully as we would do but Al may have done this or Dad may have been saving space on the page.
+            </p><p>Some letters are slightly out of date order in the books but as most of these are around the time his father died I have put them in the correct order as this is crucial to understanding the sense of the contents.  A lot will be missing – many, many ships were sunk but thanks to Aunt Alice we have these copies of the ones that did get through.
+            </p><p>Where Dad used brackets in a letter I have used [ ] style and where I have made any comment or explanation I have used ( ) and Italics.
+            </p><p>Where he uses the word “gay” it is used in the true sense, bright, happy, carefree, etc. This is the original meaning of the word before it became associated with the homosexual community.
+            </p><p>The amounts of money are, of course, in Pounds, shillings and pence but as these are out of date and we have no pound sign in the computer I have written them in, sometimes showing the decimal equivalents. One Pound equalled $2, one shilling equalled 10 cents and one penny equalled a little under one cent. After a while I stopped putting the equivalents in as they had no significance unless you knew the relative cost of things then and now. A shilling – equivalent to ten cents might actually buy ten or twenty dollars worth of goods now.
+            </p>
+        </article>
 OUTPUT;
-    echo $html;
-    //echo "<p>test".$lettersArray[$articleID]."</p>";
-}*/
+        echo $html;
+    } else if ($_GET["articleID"] === "Contact us") {
+        $html = <<<"OUTPUT"
+        <article id="form">
+            <div class="articleHeader">
+                <h2 class="basic">Contact Me</h2>
+            </div>
+            <form action="https://titan.csit.rmit.edu.au/~e54061/wp/testcontact.php" method="post">
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name" placeholder="Name">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Email">
+                <label for="mobile">Mobile</label>
+                <input type="subject" id="mobile" name="mobile" placeholder="Mobile">
+                <label for="subject">Subject</label>
+                <input type="message" id="subject" name="subject" placeholder="Subject">
+                <label class="messageField" for="message">Message</label>
+                <textarea class="messageField basic" id="message" name="message" cols='80' rows='9'></textarea>
+                <input class="checkBox" type="checkbox" id="remember-me" name="remember-me">
+                <label class="checkBoxLabel" for="remember-me">Remember me</label>
+                <input type="submit" name="send" value="Submit">
+            </form>
+        </article>
+OUTPUT;
+        echo $html;
+    } else {/*
+    $contentFormatted = str_replace("\n", "</p><p>", {$lettersArray[$_GET["articleID"]][Content]});
+        echo "<article>
+                <div class=\"articleHeader\">
+                    <h2>".{$lettersArray[$_GET["articleID"]][Battle]}."</h2>
+                    <h3>".{$lettersArray[$_GET["articleID"]][Town]}.", ".{$lettersArray[$_GET["articleID"]][Country]}." - ".{$lettersArray[$_GET["articleID"]][DateStart]}."</h3>
+                </div>
+                <div class=\"".{$lettersArray[$_GET["articleID"]][Type]}."\">
+                    <div>".{$lettersArray[$_GET["articleID"]][Type]}." placeholder cover text</div>
+                    <div><p>".{$contentFormatted}."</p></div>
+                </div>
+            </article>";*/
+    }
+}
 
-function articleBuilder($dateStart, $dateEnd, $type, $town, $country, $transport, $battle, $content) {
+/*function articleBuilder($dateStart, $dateEnd, $type, $town, $country, $transport, $battle, $content) {
     $contentFormatted = str_replace("\n", "</p><p>", $content);
     $html = <<<"OUTPUT"
                 <article>
@@ -147,6 +204,6 @@ function articleBuilder($dateStart, $dateEnd, $type, $town, $country, $transport
                 </article>
 OUTPUT;
     echo $html;
-}
+}*/
 
 ?>
